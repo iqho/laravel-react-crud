122e68d06755e52ea21a060a2fa3eb9c74f74da3
iqho <hosiqb@gmail.com>
2024-06-27 18:40:44 +0600

    Update Home Page

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,3 +1,10 @@
+const truncateText = (text, maxLength) => {
+    if (text.length <= maxLength) {
+        return text;
+    }
+    return text.substring(0, maxLength) + '...';
+};
+
 export default function Index({ posts }) {
     return (
         <>
@@ -5,9 +12,12 @@ export default function Index({ posts }) {
             <hr />
             <ul>
                 {posts.map(post => (
-                    <li key={post.id}>
-                        <a href={post.showUrl}>{post.title}</a> <br />
-                        {post.body}
+                    <li key={post.id} style={{ fontSize:'18px', marginBottom:'5px', paddingBottom:'5px' }}>
+                        <a href={post.showUrl} style={{ color: 'red' }}> {post.title} </a> <br/>
+                        <p style={{ fontSize:'16px', margin:'0px' }}>
+                        {truncateText(post.body, 50)} <br />
+                        </p>
+                        <a href={post.showUrl} style={{ color: 'blue', fontSize:'14px' }}> Read more </a>
                     </li>
                 ))}
             </ul>
